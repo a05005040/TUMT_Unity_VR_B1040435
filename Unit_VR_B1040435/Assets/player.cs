aae21539144c8ba69d9909d5102cf1f0cb5c6acf
a05005040 <55218579+a05005040@users.noreply.github.com>
2019-12-30 15:00:08 +0800

    期末作業

---
+++ Unit_VR_B1040435/Assets/player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public GameObject a, b;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ThrowPickUp()
+    {
+        print("撿起物件");
+        Destroy(a);
+        b.SetActive(true);
+
+    }
+    public void ThrowDetach()
+    {
+        print("放開物件");
+    }
+    public void ThrowHeld()
+    {
+        print("拿著物件");
+    }
+}
